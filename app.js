05b218999546e56bb2debb3a1da9522fce9ff987
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-04-20 21:13:25 +0200

    :sparkles: add socket connect/disconnect code

--- app.js
+++ app.js
@@ -23,6 +23,24 @@ var io = socketio(server);
 var api_key = process.env.API_KEY;
 var port = process.env.PORT || 3000;
 
+connections = [];
+
+io.on('connection', function(socket) {
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //Disconnect
+  socket.on('disconnect', function(data) {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log('Disconnected: %s sockets sonnected', connections.length);
+  });
+
+  //Send Message
+  socket.on('send message', function(data) {
+    io.sockets.emit('new message', {msg: data});
+  });
+});
+
 function home (req, res) {
   request('https://api.flickr.com/services/rest', {
     qs: {
@@ -32,4 +50,4 @@ function home (req, res) {
   res.render('home');
 }
 
-app.listen(port);
+server.listen(port);
