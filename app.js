41fa99a44d82549d3fb91c45941636bed1c2f59a
Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>
2017-05-03 17:36:45 +0200

    :art: add reddit
    Signed-off-by: Mirza van Meerwijk <mirzavanmeerwijk@gmail.com>

--- app.js
+++ app.js
@@ -35,36 +35,12 @@ var schema = new GraphQLSchema({
   query: Reddit.QueryObjectType
 });
 
-var query = '{ subreddit(name: "worldnews") { newListings(limit: 50) {title url}}}';
-graphql(schema, query).then(function(result) {
-  console.log(util.inspect(result, false, null))
-});
-
-connections = [];
-
-io.on('connection', function(socket) {
-  connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
-
-  //Disconnect
-  socket.on('disconnect', function(data) {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets sonnected', connections.length);
-  });
-
-  //Send Message
-  socket.on('send message', function(data) {
-    io.sockets.emit('new message', {msg: data});
-  });
-});
-
 function home (req, res) {
-  request('https://api.flickr.com/services/rest', {
-    qs: {
-
-    }
+  var query = '{ subreddit(name: "worldnews") { newListings(limit: 10) {title url}}}';
+  graphql(schema, query).then(function(result) {
+    // console.log(util.inspect(result, false, null))
+    res.render('home', {data: result});
   });
-  res.render('home');
 }
 
 server.listen(port);
